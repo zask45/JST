eaee9d2ade2117a8474e21931b96368c958f43d9
Keysha Zascha Medina <117462539+zask45@users.noreply.github.com>
2024-05-11 16:04:38 +0700

    Update GrowingNeuralGas.py

--- GrowingNeuralGas.py
+++ GrowingNeuralGas.py
@@ -7,7 +7,7 @@ https://papers.nips.cc/paper/893-a-growing-neural-gas-network-learns-topologies.
 http://www.booru.net/download/MasterThesisProj.pdf
 """
 
-from helper import Graph
+import Graph
 import numpy as np
 
 
